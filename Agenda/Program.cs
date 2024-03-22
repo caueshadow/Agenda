6511c9e37e0b18448e279e44ca8fd071f061f53e
Caue <cauepimentadasilva@gmail.com>
2024-03-21 21:36:34 -0300

    testes

--- Agenda/Program.cs
+++ Agenda/Program.cs
@@ -2,9 +2,34 @@
 {
     internal class Program
     {
+        static void ler(string arq)
+        {
+            using var ler = new StreamReader(arq);
+            string line;
+            while ((line = ler.ReadLine()) != "-")
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        static void escrever(string arq, string line)
+        {
+            StreamWriter escrever = new StreamWriter(arq);
+            string nome;
+            Console.WriteLine("Escreva o nome");
+            nome = Console.ReadLine();
+            escrever.WriteLine(nome);
+            escrever.WriteLine("-");
+            escrever.Close();
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Y:\aa.txt
+            String arq;
+            Console.WriteLine("Informe o caminho do arquivo");
+            arq = Console.ReadLine();
+            //escrever(arq);
+            ler(arq);
         }
     }
 }
